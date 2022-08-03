85e6426cc1c7a688ae9c2634fe1600c040a9dbaa
Aditya Pratap Singh Chauhan <90275944+Aditya2086@users.noreply.github.com>
2022-08-04 00:24:20 +0530

    Create Find N Unique Integers Sum up to Zero.cpp

---
+++ Find N Unique Integers Sum up to Zero.cpp
@@ -0,0 +1,13 @@
+class Solution {
+public:
+    vector<int> sumZero(int n) {
+        vector<int> result;
+        for (int i = -(n / 2); i <= (n / 2); ++i) {
+            if (i == 0 && n % 2 == 0) {
+                continue;
+            }
+            result.emplace_back(i);
+        }
+        return result;
+    }
+};
